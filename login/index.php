709e8a34bf4484e93d1a83828a7fbf115a0489c9
Gerverson Araujo <gerversonsa@gmail.com>
2018-08-17 20:40:57 -0300

    login

---
+++ login/index.php
@@ -0,0 +1,31 @@
+<link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+
+<!--Pulling Awesome Font -->
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-offset-3 col-md-4">
+            <div class="form-login">
+            <h4 align="center">Entre no Sistema</h4>
+            <label>Email:</label><br>
+            <input type="mail" id="userName" class="form-control input-sm chat-input" placeholder="username" />
+            </br>
+            <label>Senha:</label><br>
+            <input type="password" id="userPassword" class="form-control input-sm chat-input" placeholder="password"/>
+            </br>
+            <div class="wrapper">
+            <span class="group-btn">
+               <div class="col-md-4 center-block"></div>
+              <a href="#" class="btn btn-primary btn-md">login <i class="fa fa-sign-in"></i></a>
+            </span>
+            </div>
+            </div>
+        
+        </div>
+    </div>
+</div>
