f0526b844bd0ba1853eb53f95add2ad3902f10dc
Gerverson Araujo <gerversonsa@gmail.com>
2018-08-17 21:34:57 -0300

    new

---
+++ confirmar/index.php
@@ -0,0 +1,4 @@
+<?php 
+include "../navbar/nav.php";
+ ?>
+<h3>Seu cadastro foi efetivado, <a href="../login">Clique aqui para entar</a></h3>
---
+++ register/index.php
@@ -0,0 +1,45 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+    <div class="container col-md-offset-3 col-md-4">
+		<div class="row">
+			<div class="panel panel-primary">
+				<div class="panel-body">
+					<form method="POST" action="#" role="form">
+						<div class="form-group">
+							<h2>Criar Conta</h2>
+						</div>
+						<div class="form-group">
+							<label class="control-label" for="signupName">Nome Completo:</label>
+							<input id="signupName" type="text" maxlength="50" class="form-control">
+						</div>
+						<div class="form-group">
+							<label class="control-label" for="signupEmail">E-mail:</label>
+							<input id="signupEmail" type="email" maxlength="50" class="form-control">
+						</div>
+						<div class="form-group">
+							<label class="control-label" for="signupEmailagain">Confirmar E-mail</label>
+							<input id="signupEmailagain" type="email" maxlength="50" class="form-control">
+						</div>
+						<div class="form-group">
+							<label class="control-label" for="signupPassword">Senha:</label>
+							<input id="signupPassword" type="password" maxlength="25" class="form-control" placeholder="at least 6 characters" length="40">
+						</div>
+						<div class="form-group">
+							<label class="control-label" for="signupPasswordagain">Repetir Senha</label>
+							<input id="signupPasswordagain" type="password" maxlength="25" class="form-control">
+						</div>
+						<div class="form-group">
+							<button id="signupSubmit" type="submit" class="btn btn-info btn-block">Create your account</button>
+						</div>
+						<p class="form-group">By creating an account, you agree to our <a href="#">Terms of Use</a> and our <a href="#">Privacy Policy</a>.</p>
+						<hr>
+						<p></p>Already have an account? <a href="#">Sign in</a></p>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+
